69c9d5849dfd535c163c7278301115e6590c0b24
Gage Olson <gage.a.olson@gmail.com>
2024-11-14 23:24:16 -0600

    Select Lang Dropdown Implementation

--- website/views.py
+++ website/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'lua', 'markup', 'markup-templating',
+                 'matlab', 'mongodb', 'objectivec', 'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sass', 'scala', 'sql', 'swift', 'yaml']
+    return render(request, 'home.html', {'lang_list': lang_list})
