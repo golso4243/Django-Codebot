5eb7ac793cb33a79ebcd71878e8eb3f71c0e1a2f
Gage Olson <gage.a.olson@gmail.com>
2024-11-14 18:38:45 -0600

    Homepage Creation

--- codebot/settings.py
+++ codebot/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'website',  # Add the website app to the list of installed apps
 ]
 
 MIDDLEWARE = [
--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path  # Import the path function from django.urls
+from . import views  # Import the views module from the current directory
 
 urlpatterns = [
-
+    path('', views.home, name='home'),  # This is the home page
 ]
