7c5f9f2b9a08646b5c090da608f015fd1d46a11c
Gage Olson <gage.a.olson@gmail.com>
2024-11-15 07:58:03 -0600

    Form Actions

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Home View - This is the home page
 
@@ -6,4 +7,13 @@ from django.shortcuts import render
 def home(request):
     lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'lua', 'markup', 'markup-templating',
                  'matlab', 'mongodb', 'objectivec', 'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sass', 'scala', 'sql', 'swift', 'yaml']
+
+    if request.method == "POST":
+        code = request.POST['code']
+        lang = request.POST['lang']
+
+        if lang == 'Select Programming Language':
+            messages.error(request, 'Please select a language')
+            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+        return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
     return render(request, 'home.html', {'lang_list': lang_list})
